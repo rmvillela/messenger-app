bc3d84ab939d733e04d8dd0b1acc302f077dbd57
Roosevelt Villela <roosevelt@peopleinteractive.com.br>
2018-12-03 18:08:43 -0200

    Integração da tela de cadastro com as actions do redux

--- src/screens/CadastroScreen.js
+++ src/screens/CadastroScreen.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { View, TextInput, Button } from 'react-native';
 import { connect } from 'react-redux';
 
+import { modificarNome, modificarEmail, modificarSenha } from '../actions/AutenticacaoActions';
+
 const CadastroScreen = props => (
   <View style={{ flex: 1, padding: 10 }}>
     <View style={{ flex: 4, justifyContent: 'center' }}>
-      <TextInput value={props.nome} placeholder="Nome" style={{ height: 45, fontSize: 20, borderBottomColor: "#000", borderBottomWidth: 1 }} />
-      <TextInput value={props.email} placeholder="E-mail" style={{ height: 45, fontSize: 20, borderBottomColor: "#000", borderBottomWidth: 1 }} />
-      <TextInput value={props.senha} placeholder="Senha" style={{ height: 45, fontSize: 20, borderBottomColor: "#000", borderBottomWidth: 1 }} />
+      <TextInput value={props.nome} placeholder="Nome" style={{ height: 45, fontSize: 20, borderBottomColor: "#000", borderBottomWidth: 1 }} onChangeText={texto => props.modificarNome(texto)}/>
+      <TextInput value={props.email} placeholder="E-mail" style={{ height: 45, fontSize: 20, borderBottomColor: "#000", borderBottomWidth: 1 }} onChangeText={texto => props.modificarEmail(texto)}/>
+      <TextInput value={props.senha} placeholder="Senha" style={{ height: 45, fontSize: 20, borderBottomColor: "#000", borderBottomWidth: 1 }} onChangeText={texto => props.modificarSenha(texto)}/>
     </View>
 
     <View style={{ flex: 1 }}>
@@ -24,4 +26,4 @@ const mapStateToProps = state => (
   }
 )
 
-export default connect(mapStateToProps, null)(CadastroScreen)
+export default connect(mapStateToProps, { modificarNome, modificarEmail, modificarSenha})(CadastroScreen)
